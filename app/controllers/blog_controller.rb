5e680818253237fac98d69044a79d28d3ed896b9
renitagid <renita.y.ip@gmail.com>
2022-12-12 13:31:29 -0800

    User Story 3 complete

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,7 +2,13 @@ class BlogController < ApplicationController
     def index
         @blogs = Blog.all 
     end 
-        def show 
+
+    def show 
         @blog = Blog.find(params[:id])
-        end 
+    end 
+
+    def new
+        @blog = Blog.new
+    end
+
 end
