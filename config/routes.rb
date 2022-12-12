039bb23b8fd9d19ae05302de0df026fb5db46a1c
ajgahan19 <andregahan77@gmail.com>
2022-12-12 11:54:22 -0800

    Second Story" git commit -m Second Story

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "blog#index"
+  root "blog#index", as:'index'
+  get 'blogs/:id' => 'blog#show', as:"blog"
 end
